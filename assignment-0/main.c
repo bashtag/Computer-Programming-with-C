f343791d87e2fe8b6c2c6fedac4f803d3ec592d1
bashtag <btstsiy@hotmail.com>
2022-03-02 17:13:58 +0300

    assignment-0 updated

--- assignment-0/main.c
+++ assignment-0/main.c
@@ -4,8 +4,49 @@
 
 int main() {
 
+	/* A fractional number: 13/7 */
+    int num1 = 13, den1 = 7;
+    /* A fractional number: 13/7 */
+    int num2 = 30, den2 = 11;
+    /* An unitilized fractional number */
+    int num3, den3;
+
+    printf("First number: ");
+    fraction_print(num1, den1);
+    printf("\n");
+
+    printf("Second number: ");
+    fraction_print(num2, den2);
+    printf("\n");
+
+    printf("Addition: ");
+    fraction_add(num1, den1, num2, den2, &num3, &den3);
+    fraction_print(num3, den3);
+    printf("\n");
+
+    printf("Subtraction: ");
+    fraction_sub(num1, den1, num2, den2, &num3, &den3);
+    fraction_print(num3, den3);
+    printf("\n");
+
+    printf("Multiplication: ");
+    fraction_mul(num1, den1, num2, den2, &num3, &den3);
+    fraction_print(num3, den3);
+    printf("\n");
+
+    printf("Division: ");
+    fraction_div(num1, den1, num2, den2, &num3, &den3);
+    fraction_print(num3, den3);
+    printf("\n");
+
+    /*
+     TODO: Complete this code to read two fractional numbers and print their 
+             multiplication and division results...
+    */
+	
     /* A fractional number */
-	int num1, den1 = 0;
+	num1 = 0;
+	den1 = 0;
 	printf("Please give me numerator of first number: ");
 	scanf("%d", &num1);
 	while (den1 == 0)
@@ -14,7 +55,8 @@ int main() {
 		scanf("%d", &den1);
 	}
 	/* A fractional number */
-    int num2, den2 = 0;
+    num2 = 0;
+	den2 = 0;
 	printf("Please give me numerator of second number: ");
 	scanf("%d", &num2);
 	while (den2 == 0)
@@ -22,8 +64,6 @@ int main() {
    		printf("Please give me denominator of second number: ");
 		scanf("%d", &den2);
 	}
-	/* An unitilized fractional number */
-    int num3, den3;
 
     printf("First number: ");
     fraction_print(num1, den1);
@@ -54,9 +94,5 @@ int main() {
 		fraction_print(num3, den3);
     printf("\n");
 
-    /*
-     TODO: Complete this code to read two fractional numbers and print their 
-             multiplication and division results...
-    */
     return(0);
 }
